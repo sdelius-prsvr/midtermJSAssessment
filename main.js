f6f5e20d8f7e9b16ba3451b6c928902e08070e09
stephen <sdelius.prsvr@gmail.com>
2023-10-23 11:27:03 -0500

    nonsense

--- main.js
+++ main.js
@@ -2,8 +2,89 @@
 // │ Your code here! │	
 // └─────────────────┘
 
+const ageToAbilities = (age) => {
+    let message
+    switch (true) {
+        case (age < 16) :
+            message = "You can't drive.";
+            break;
+        case (age < 18) :
+            message = "You can drive but not vote.";
+            break;
+        case (age < 25) :
+            message = "You can vote but not rent a car.";
+            break;
+        case (age >= 25) :
+            message = "You can do pretty much anything.";
+            break;
+    }
+    return message
+}
+
+const oddIndices = (arr) => {
+    let newArr = []
+    for (let i = 1; i < arr.length; i += 2) {
+        newArr.push(arr[i])
+    }
+    return newArr
+}
+
+const numOddValues = (arr) => {
+    let count = 0
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] % 2 !== 0){
+            count ++
+        }
+    }
+    return count
+}
+
+const averageStringLength = (arr) => {
+    if (arr.length > 0){
+    let total = 0
+    for (let i = 0; i < arr.length; i++){
+        total += arr[i].length
+    }
+    return (total/(arr.length))
+}
+return 0
+}
+
+const  firstPunctuationIndex = (str) => {
+    let result = -1
+    for (let i = 0; i < str.length; i++) {
+        if (str.charAt(i) === '.' || str.charAt(i) === '?' || str.charAt(i) === '!')
+        return result = i
+    }
+    return result
+}
+
+const getPlace = (highScores, score) => {
+    let place = 0
+    for (let i = 0; i < highScores.length; i++){
+        if (score > highScores[i]) {
+            place = i + 1
+        }
+    }    
+    if (place[(place.length)] === 0){
+        return `${place}st place`
+    }if (place[(place.length)] === 1){
+        return `${place}nd place`
+    }if (place[(place.length)] === 2){
+        return `${place}rd place`
+    }else{
+        return `${place}th place`
+    }
+}
 
 
+const addToObj = (person, key, value) => {
+    if (typeof key === 'string'){
+    person.key = value;
+    return
+    }
+    else return "Function must be called with a valid key."
+}
 
 // ┌─────────────────────────────────────┐
 // │ Do not modify code below this line. │
